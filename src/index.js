3e31da96390bbffbd1b977ed40f91aadfcb07231
solomon-cbs <solomon@cbs.com.mm>
2026-06-01 22:44:35 +0700

    [moidy] vercel 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import express from 'express';
 import { createApp } from './httpApp.js';
 import { bootstrap } from './bootstrap.js';
 
 bootstrap();
 
-export default createApp();
+const app = createApp();
+
+export default app;
